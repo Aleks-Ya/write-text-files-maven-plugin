e2d417bd95bb36f2d91e164a3368e3375367814f
Aleksey Yablokov <alex_ya@mailbox.org>
2025-10-18 17:16:06 +0800

    build.sh does not use ~/.m2 because it writes files as the root user, which leads to access issues for other users

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 MAVEN_VERSION="${1:-3.6.3}"
 
 docker run -it --rm \
-	-v "$HOME/.m2":/root/.m2 \
+	-v "/tmp/write-text-files-maven-plugin/.m2":/root/.m2 \
 	-v "$PWD":/var/project \
 	-w /var/project \
 	maven:${MAVEN_VERSION} \
